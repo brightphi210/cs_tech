9198a6de2ce0be608e3f5f5d9c1aa0a4ff0e9ed3
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-12-07 07:18:25 -0800

    latest

--- src/Components/HomeCompo/SectionTwo.jsx
+++ src/Components/HomeCompo/SectionTwo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import imga from './images/a (1).png'
 import imgb from './images/a (2).png'
 import imgc from './images/a (3).png'
+import imgd from './images/d.jpg'
 
 
 import './SectionTwo.scss'
@@ -16,16 +17,50 @@ const SectionTwo = () => {
                 <div className='weDiv'>
                     <img src={imga} alt="" />
                     <button>Web 1</button>
+                    <p>
+                    Web 2.0 represents a user-centric, collaborative, 
+                    and dynamic approach to the web, 
+                    fostering a more interactive and participatory online experience.
+                    </p>
                 </div>
 
                 <div className='weDiv'>
                     <img src={imgb} alt="" />
                     <button>Web 2</button>
+                    <p>
+                        Web 2.0 represents a user-centric, collaborative, 
+                        and dynamic approach to the web, 
+                        fostering a more interactive and participatory online experience.
+                    </p>
                 </div>
 
                 <div className='weDiv'>
                     <img src={imgc} alt="" />
                     <button>Web 3</button>
+                    <p>
+                        Web 2.0 represents a user-centric, collaborative, 
+                        and dynamic approach to the web, 
+                        fostering a more interactive and participatory online experience.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+        <div className='sectionTwoOne'>
+            <h2>Development Infrastructures : </h2>
+            <div className='sectionTwoFlex'>
+                <div className='weDivOne'>
+                    <img src={imgd} alt="" />
+                </div>
+
+                <div className='weDivOne'>
+                    <img src={imgd} alt="" />
+                </div>
+
+
+                <div className='weDivOne'>
+                    <img src={imgd} alt="" />
                 </div>
             </div>
         </div>
