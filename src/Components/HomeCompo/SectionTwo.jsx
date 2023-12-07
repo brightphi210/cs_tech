c2f5d1b4a36a113b80a412e29bbb1b860b2b101e
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-12-07 07:35:05 -0800

    latest

--- src/Components/HomeCompo/SectionTwo.jsx
+++ src/Components/HomeCompo/SectionTwo.jsx
@@ -52,15 +52,32 @@ const SectionTwo = () => {
             <div className='sectionTwoFlex'>
                 <div className='weDivOne'>
                     <img src={imgd} alt="" />
+                    <h1>Bitsave:</h1>
+                    <h2>
+                        We built a multi-chain Savings protocol 
+                        from scratch, 
+                        the project involved:
+                    </h2>
+                    <p>Research Back end developments</p>
+                    <p>Maths and economic formula developments</p>
+                    <p>Pseudo code creation</p>
+                    <p>Flow chart development</p>
+                    <p>UI/UX design</p>
+                    <p>Smart Contract development</p>
+                    <p>Front End developments</p>
                 </div>
 
                 <div className='weDivOne'>
                     <img src={imgd} alt="" />
-                </div>
+                    <h1>Emilist:</h1>
+                    <h2>
+                        Emilist is a service hiring platform 
+                        with a suite of three applications:
+                    </h2>
+                    <p>Emilist Web app (Landing page and user dashboard/area)</p>
+                    <p>Emilist Admin Dashboard</p>
+                    <p>Emilist Cross Platform Mobile App</p>
 
-
-                <div className='weDivOne'>
-                    <img src={imgd} alt="" />
                 </div>
             </div>
         </div>
