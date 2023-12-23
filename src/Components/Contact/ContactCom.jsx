98c11465321879526251a413c985e5f4564e2f88
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2023-12-23 10:27:05 -0800

    a

--- src/Components/Contact/ContactCom.jsx
+++ src/Components/Contact/ContactCom.jsx
@@ -123,7 +123,7 @@ const ContactCom = () => {
                   id="text"
                   name="category1"
                 
-                placeholder='Category 1 e.g Academy Category ' />
+                placeholder='Category 1 e.g Academy ' />
 
             </div>
 
@@ -133,7 +133,7 @@ const ContactCom = () => {
                   type="text" 
                   id="text"
                   name="category2"
-                placeholder='Category 2 e.g Product Development Category ' />
+                placeholder='Category 2 e.g Product Development ' />
 
             </div>
 
